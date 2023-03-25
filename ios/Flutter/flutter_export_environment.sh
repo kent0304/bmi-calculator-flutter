6472e3745cd5f8a765f1a6d199043aac15980456
kent0304 <tanaken0304.0123@gmail.com>
2023-03-25 21:12:42 +0900

    fluttter pub get

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tanakakent/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tanakakent/Desktop/projects/flutter/bmi-calculator-flutter"
+export "FLUTTER_ROOT=/Users/01046457/Desktop/projects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/01046457/Desktop/projects/flutter/my_project/bmi-calculator-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/tanakakent/Desktop/projects/flutter/bmi-calculator-flutter/lib/main.dart"
+export "FLUTTER_TARGET=/Users/01046457/Desktop/projects/flutter/my_project/bmi-calculator-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/tanakakent/Desktop/projects/flutter/bmi-calculator-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/01046457/Desktop/projects/flutter/my_project/bmi-calculator-flutter/.dart_tool/package_config.json"
